c0367f22ef2268ae2664ebb88a23982595f82d61
another_xq <anohter_xiaoqiang@163.com>
2019-07-05 16:51:51 +0800

    Query String and URL parameters

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,17 +5,8 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage'
+import Header from '../components/Header';
 import 'normalize.css/normalize.css';
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-  </header>
-);
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -24,7 +15,7 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
