afd5f5c4584a0026e5420a210f82907f2cbb42ea
another_xq <anohter_xiaoqiang@163.com>
2019-07-09 12:58:56 +0800

    add header order component demo to playground

---
+++ src/playground/hoc.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>This info is: {props.info}</p>
+  </div>
+);
+
+const withAdminWarning = (WrappedComponent)=> {
+  return (props) => (
+    <div>
+      {props.isActive && <p>This is priviate info, Please don't share it!</p> }
+      <WrappedComponent {...props} />
+    </div>
+  )
+};
+
+// requireAuthentication
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAuthenticated ? ( <WrappedComponent {...props} />) : ( <p>Please login to review the info</p> )}
+    </div>
+  )
+}
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="these are the details" />, document.getElementById("app"));
