71d0467be6a3320382cc819cf28fcbeb20fc11d9
xiaoqiangliu <xiaoqiang.liu@yahoo.com>
2019-07-11 06:31:45 +0800

    wiring up Edit expense

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -10,7 +10,8 @@ export default class ExpenseForm extends React.Component {
     note: '',
     amount: '',
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ''
   };
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -29,13 +30,29 @@ export default class ExpenseForm extends React.Component {
   onDateChange = (createdAt) => {
     this.setState(() => ({ createdAt }));
   };
-  onFocusChange = ({focused}) => {
-    this.setState(() => ({ calendarFocused: focused}))
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }))
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Please provide description and amount.' }));
+    } else {
+      this.setState(()=> ({error: ''}));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      })
+    }
   };
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
@@ -55,7 +72,7 @@ export default class ExpenseForm extends React.Component {
             focused={this.state.calendarFocused}
             onFocusChange={this.onFocusChange}
             numberOfMonths={1}
-            isOutsideRange={()=>false}
+            isOutsideRange={() => false}
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
