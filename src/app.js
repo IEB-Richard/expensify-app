ff0fd19e147728929663dd348ba8ca94e9eb1d71
xiaoqiangliu <xiaoqiang.liu@yahoo.com>
2019-07-11 05:28:27 +0800

    add date picker

--- src/app.js
+++ src/app.js
@@ -10,17 +10,18 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill' }));
-store.dispatch(addExpense({description: 'Gas bill' }));
+store.dispatch(addExpense({description: 'Water bill', amount: 4500 }));
+store.dispatch(addExpense({description: 'Gas bill', amount: 1000 }));
+store.dispatch(addExpense({description: 'Rent', amount: 109500 }));
 store.dispatch(setTextFilter( 'water' ));
 
 setTimeout(()=>{
   store.dispatch(setTextFilter( 'Gas' ));
 }, 3000)
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
