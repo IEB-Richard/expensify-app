20888a4a0af9485d39d9905a696c30cc993151dd
another_xq <anohter_xiaoqiang@163.com>
2019-07-11 20:25:08 +0800

    commit before sync

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => {
   console.log(props);
   return (
     <div>
       This is from my edit expense component of id {props.match.params.id}
+      <ExpenseForm 
+        expense={props.expense}
+        onSubmit={(expense)=>{
+          props.dispatch()
+        }}
+      />
     </div>
   );
 };
 
-export default EditExpensePage;
+export default connect()(EditExpensePage);
