0cca4f709b5a25f78c14c2b799529fe22bf61e28
xiaoqiangliu <xiaoqiang.liu@yahoo.com>
2019-07-17 07:04:59 +0800

    snapshot testing with dynamic components part 01

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactShallowReander from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+// import toJSON from 'enzyme-to-json';
+// import ReactShallowReander from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowReander();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  // const renderer = new ReactShallowReander();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+  const wrapper = shallow(<Header />);
+  // expect(wrapper.find('h1').text()).toBe("Expensify");
+  // expect(toJSON(wrapper)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
